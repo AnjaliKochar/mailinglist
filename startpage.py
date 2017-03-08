945d020c6aafef623063f830cc325d1874a0f895
deepak <deepakk@letsrundigital.com>
2017-03-08 13:07:19 +0530

    Added the redirection for admin link in subscribe page.

--- startpage.py
+++ startpage.py
@@ -1,7 +1,16 @@
 from flask import Flask,render_template
+
 app=Flask(__name__)
+
+
 @app.route('/')
 def firstpage():
     return render_template('Subscribe.html')
+
+
+@app.route('/admin_login')
+def admin_login():
+    return render_template('adminlogin.html')
+
 if __name__=='__main__':
     app.run(host='127.0.0.1',port=4444,debug=True)
