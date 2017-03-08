77eb7453e03b1965bedc83321906c13c6b9a2b2c
anjali kochar <anjali kochar>
2017-03-08 14:37:14 +0530

    curator and submitter login

--- displaydetails.py
+++ displaydetails.py
@@ -8,6 +8,13 @@ app.config.from_object(__name__)
 def connect_database():
     return sqlite3.connect(app.config['DATABASE'])
 
+@app.route('/')
+def firstpage():
+    return render_template('Subscribe.html')
+
+@app.route('/admin_login')
+def admin_login():
+     return render_template('adminlogin.html')
 
 
 @app.route('/details')
@@ -21,6 +28,8 @@ def subdetails():
 @app.route('/updateddetail')
 def updatepage():
     return render_template('assignnewsubmitter.html')
+
+
 @app.route('/updateddetail',methods=['get','post'])
 def detail():
     g.db=connect_database()
@@ -37,11 +46,24 @@ def detail():
     c=g.db.execute('insert into users values(?,?,?,?,?,?,?)',mlistt)
     c=g.db.execute('select user_name,email,phoneno,company_designation,list_role,subscribed_list,password from users')
     g.db.commit()
-    details = [dict(user_name=row[0], email=row[1], phoneno=row[2], company_designation=row[3],list_role=row[4],subscribed_list=row[5],password=row[6]) for row in c.fetchall()]
+    details = [dict(user_name=row[0], email=row[1], phoneno=row[2], company_designation=row[3],list_role=row[4],
+                    subscribed_list=row[5],password=row[6]) for row in c.fetchall()]
     g.db.close()
     return render_template('login2.html', details=details)
 
 
+@app.route('/curatororsub_login')
+def curatororsub_login():
+    g.db = connect_database()
+    profile = request.form['cors']
+    aemail = request.form['adminemail']
+    apswd = request.form['adminpswd']
+    print("hi")
+    c=g.db.execute('select user_name,email,phoneno,company_designation,list_role,subscribed_list,password from users where (list_role=profile AND aemail=email AND apswd=password)')
+    details = [dict(user_name=row[0], email=row[1], phoneno=row[2], company_designation=row[3], list_role=row[4],
+                    subscribed_list=row[5], password=row[6]) for row in c.fetchall()]
+    g.db.close()
+    return render_template('login2.html', details=details)
 
 if __name__=='__main__':
     app.run(host='127.0.0.1',port=4444,debug=True)
--- startpage.py
+++ startpage.py
@@ -3,5 +3,9 @@ app=Flask(__name__)
 @app.route('/')
 def firstpage():
     return render_template('Subscribe.html')
+
+@app.route('/admin_login')
+def admin_login():
+     return render_template('adminlogin.html')
 if __name__=='__main__':
     app.run(host='127.0.0.1',port=4444,debug=True)
