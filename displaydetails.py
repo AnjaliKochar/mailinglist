6c0084ce767e44c778bdc3aad2239c4851e6d734
anjali kochar <anjali kochar>
2017-03-08 12:02:10 +0530

    startpage

--- displaydetails.py
+++ displaydetails.py
@@ -1,4 +1,4 @@
-from flask import Flask,g,render_template
+from flask import Flask,g,render_template,request
 import sqlite3
 
 app=Flask(__name__)
@@ -7,6 +7,11 @@ app.config.from_object(__name__)
 
 def connect_database():
     return sqlite3.connect(app.config['DATABASE'])
+
+@app.route('/')
+def firstpage():
+    return render_template('Subscribe.html')
+
 @app.route('/details')
 def subdetails():
     g.db=connect_database()
@@ -15,5 +20,30 @@ def subdetails():
     g.db.close()
     return render_template('login2.html', details=details)
 
+@app.route('/updateddetail')
+def updatepage():
+    return render_template('assignnewsubmitter.html')
+@app.route('/updateddetail',methods=['get','post'])
+def detail():
+    g.db=connect_database()
+
+    name=request.form['nme']
+    mail=request.form['mail']
+    phone=request.form['number']
+    post=request.form['post']
+    role=request.form['role']
+    password=request.form['pswrd']
+    conpswrd=request.form['conpswrd']
+    subslist='none'
+    mlistt=((name,mail,phone,post,role,subslist,password))
+    c=g.db.execute('insert into users values(?,?,?,?,?,?,?)',mlistt)
+    c=g.db.execute('select user_name,email,phoneno,company_designation,list_role,subscribed_list,password from users')
+    g.db.commit()
+    details = [dict(user_name=row[0], email=row[1], phoneno=row[2], company_designation=row[3],list_role=row[4],subscribed_list=row[5],password=row[6]) for row in c.fetchall()]
+    g.db.close()
+    return render_template('login2.html', details=details)
+
+
+
 if __name__=='__main__':
-    app.run(debug=True)
+    app.run(host='127.0.0.1',port=4444,debug=True)
