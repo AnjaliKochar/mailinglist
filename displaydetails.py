e1436966b52f50c5041e133454d66eff595e94af
anjali kochar <anjali kochar>
2017-03-17 16:30:30 +0530

    updated database

--- displaydetails.py
+++ displaydetails.py
@@ -21,27 +21,7 @@ def admin_login():
 def assign_newsubmitter():
     return render_template('assignnewsubmitter.html')
 
-@app.route('/curatoroptionsassignnew')
-def assign_newsubmittermethod():
-    g.db = connect_database()
-
-    name = request.form['nme']
-    mail = request.form['mail']
-    phone = request.form['number']
-    post = request.form['post']
-    role = request.form['role']
-    password = request.form['pswrd']
-    conpswrd = request.form['conpswrd']
-    subslist = 'none'
-    mlistt = ((name, mail, phone, post, role, subslist, password))
-    c = g.db.execute('insert into users values(?,?,?,?,?,?,?)', mlistt)
-    c = g.db.execute('select user_name,email,phoneno,company_designation,list_role,subscribed_list,password from users')
-    g.db.commit()
-    details = [dict(user_name=row[0], email=row[1], phoneno=row[2], company_designation=row[3], list_role=row[4],
-                    subscribed_list=row[5], password=row[6]) for row in c.fetchall()]
-    g.db.close()
-    return render_template('login2.html', details=details)
-
+#/curatoroptionsupdateprofile' used for submitter profile update too
 @app.route('/curatoroptionsupdateprofile')
 def update_profile():
     return render_template('updateprofile.html')
@@ -54,20 +34,56 @@ def send_newsletter():
 def suggest_link():
     return render_template('submitterlinksuggest.html')
 
-@app.route('/details')
-def subdetails():
-    g.db=connect_database()
-    c=g.db.execute('select user_name,email,phoneno,company_designation,list_role,subscribed_list,password from users')
-    details = [dict(user_name=row[0], email=row[1], phoneno=row[2], company_designation=row[3],list_role=row[4],subscribed_list=row[5],password=row[6]) for row in c.fetchall()]
+
+@app.route('/curatororsub_login' ,methods=['get','post'])
+def curatororsub_login():
+
+    g.db = connect_database()
+
+    profile = request.form['cors']
+    aemail = request.form['adminemail']
+    apswd = request.form['adminpswd']
+    detaillist=((profile,aemail,apswd))
+    c=g.db.execute('select user_name from users where list_role=? AND email=? AND password=?', detaillist)
+    details = [dict(user_name=row[0]) for row in c.fetchall()]
+    if len(details) > 0 and profile=='Submitter':
+        g.db.close()
+        return render_template('submitteroptions.html')
+    elif len(details) > 0 and profile=='Curator':
+        g.db.close()
+        return render_template('curatoroptions.html')
+    else:
+        flash('sorry incorrect details !!')
+        g.db.close()
+        return render_template('adminlogin.html')
+
+
+
+
+@app.route('/curatoroptionsassignnew',methods=['get','post'])
+def assign_newsubmittermethod():
+    g.db = connect_database()
+
+    name = request.form['nme']
+    mail = request.form['mail']
+    phone = request.form['number']
+    post = request.form['post']
+    role = request.form['role']
+    password = request.form['pswrd']
+    conpswrd = request.form['conpswrd']
+
+    mlistt = ((name, mail, phone, post, role, password))
+
+    c = g.db.execute('insert into users values(?,?,?,?,?,?,?)', mlistt)
+    c = g.db.execute('select user_name,email,phoneno,company_designation,list_role,subscribed_list,password from users')
+
+    g.db.commit()
+    details = [dict(user_name=row[0], email=row[1], phoneno=row[2], company_designation=row[3], list_role=row[4],
+                    password=row[5]) for row in c.fetchall()]
     g.db.close()
-    return render_template('login2.html', details=details)
+    return render_template('newsubmitterdisplay.html', details=details)
 
-@app.route('/updateddetail')
-def updatepage():
-    return render_template('assignnewsubmitter.html')
-
-
-@app.route('/updateddetail',methods=['get','post'])
+@app.route('/curatoroptionsupdateprofile',methods=['get','post'])
 def detail():
     g.db=connect_database()
 
@@ -86,30 +102,18 @@ def detail():
     details = [dict(user_name=row[0], email=row[1], phoneno=row[2], company_designation=row[3],list_role=row[4],
                     subscribed_list=row[5],password=row[6]) for row in c.fetchall()]
     g.db.close()
+
     return render_template('login2.html', details=details)
 
 
-@app.route('/curatororsub_login' ,methods=['get','post'])
-def curatororsub_login():
+@app.route('/details')
+def subdetails():
+    g.db=connect_database()
 
-    g.db = connect_database()
-    profile = request.form['cors']
-    aemail = request.form['adminemail']
-    apswd = request.form['adminpswd']
-    detaillist=((profile,aemail,apswd))
-    c=g.db.execute('select * from users where list_role=? AND email=? AND password=?', detaillist)
-    details = [dict(user_name=row[0], email=row[1], phoneno=row[2], company_designation=row[3], list_role=row[4],
-                 subscribed_list=row[5], password=row[6]) for row in c.fetchall()]
-    if len(details) > 0 and profile=='Submitter':
-        g.db.close()
-        return render_template('submitteroptions.html')
-    elif len(details) > 0 and profile=='Curator':
-        g.db.close()
-        return render_template('curatoroptions.html')
-    else:
-        flash('sorry incorrect details !!')
-        g.db.close()
-        return render_template('adminlogin.html')
+    c=g.db.execute('select user_name,email,phoneno,company_designation,list_role,subscribed_list,password from users')
+    details = [dict(user_name=row[0], email=row[1], phoneno=row[2], company_designation=row[3],list_role=row[4],subscribed_list=row[5],password=row[6]) for row in c.fetchall()]
+    g.db.close()
+    return render_template('login2.html', details=details)
 
 
 
--- sql.py
+++ sql.py
@@ -1,20 +1,52 @@
 import sqlite3
 import  sys
-mlist=(('John','john@lrd',233,'Developer','Submitter','Developer','abc123'),
-       ('John','john@lrd',233,'Developer','Submitter','SEO','abc123'),
-       ('Marry','marry@lrd',556,'Content Writer','Subscriber','Content Writer','abc123'),
-       ('Cherry','cherry@lrd',356,'SEO','Curator','SEO','abc123'),
-       ('Nisha','nisha@lrd',556,'DMM','Submitter','DMM','abc123'),
-       ('Neha','neha@lrd',226,'UI/UX','Submitter','UI/UX','abc123'),
-       ('Harry','harry@lrd',116,'Graphic Designers','Subscriber','Graphic Designer','abc123'),
-       ('Om', 'om@lrd', 566, 'Database','Submitter','Database','abc123'))
-#con=sqlite3.connect('sales.db')
-#mlist=(('johnid',233),('marryid',556))
+
+ulist=((1,'John','john@lrd',233,'Developer','Submitter','abc123'),
+       (2,'Marry','marry@lrd',556,'Content Writer','Subscriber','abc123'),
+       (3,'Cherry','cherry@lrd',356,'SEO','Curator','abc123'),
+       (4,'Nisha','nisha@lrd',556,'DMM','Submitter','abc123'),
+       (5,'Neha','neha@lrd',226,'UI/UX','Submitter','abc123'),
+       (6,'Harry','harry@lrd',116,'Graphic Designer','Subscriber','abc123'),
+       (7,'Om', 'om@lrd', 566, 'Database','Submitter','abc123'))
+
+
+llist=((1,'Developer'),(2,'ContentWriter'),(3,'SEO'),(4,'DMM'),(5,'UIUX'),(6,'GraphicDesigner'),(7,'Database'))
 
 with sqlite3.connect("mailinglist.db") as connection:
     c=connection.cursor()
     c.execute('DROP TABLE IF EXISTS users')
-    c.execute("CREATE TABLE users(user_name TEXT,email TEXT,phoneno INTEGER,company_designation TEXT,list_role TEXT,subscribed_list TEXT,password TEXT,PRIMARY KEY(email,subscribed_list))")
-   # c.execute("CREATE TABLE users(user_id TEXT,user_name INTEGER)")
+    c.execute("CREATE TABLE users(user_id INTEGER,user_name TEXT,email TEXT,phoneno INTEGER,"
+              "company_designation TEXT,list_role TEXT,password TEXT,PRIMARY KEY(user_id))")
+    c.executemany('insert into users values (?,?,?,?,?,?,?)',ulist)
 
-    c.executemany('insert into users values (?,?,?,?,?,?,?)',mlist)
+    c.execute('DROP TABLE IF EXISTS lists')
+    c.execute("CREATE TABLE lists(list_id INTEGER,list_name TEXT,PRIMARY KEY(list_id))")
+    c.executemany('insert into lists values (?,?)',llist)
+
+    c.execute('DROP TABLE IF EXISTS subscriber_lists')
+    c.execute("CREATE TABLE subscriber_lists(user_id INTEGER ,list_id INTEGER,"
+              "FOREIGN KEY(list_id) REFERENCES lists(list_id),FOREIGN KEY (user_id) REFERENCES users(user_id),"
+              "PRIMARY KEY(user_id,list_id))")
+
+
+
+
+"""mlist=(('John','john@lrd',233,'Developer','Submitter','abc123',1,0,0,0,0,0,0),
+       ('Marry','marry@lrd',556,'Content Writer','abc123','Subscriber',0,1,0,0,0,0,0),
+       ('Cherry','cherry@lrd',356,'SEO','Curator','abc123',0,0,1,0,0,0,0),
+       ('Nisha','nisha@lrd',556,'DMM','Submitter','abc123',0,0,0,1,0,0,0),
+       ('Neha','neha@lrd',226,'UI/UX','Submitter','abc123',0,0,0,0,1,0,0),
+       ('Harry','harry@lrd',116,'Graphic Designer','abc123','Subscriber',0,0,0,0,0,1,0),
+       ('Om', 'om@lrd', 566, 'Database','Submitter','abc123',0,0,0,0,0,0,1))
+
+
+with sqlite3.connect("mailinglist.db") as connection:
+    c=connection.cursor()
+    c.execute('DROP TABLE IF EXISTS users')
+    c.execute("CREATE TABLE users(user_name TEXT,email TEXT,phoneno INTEGER,"
+              "company_designation TEXT,list_role TEXT,password TEXT,"
+              "Developer INTEGER,Content Writer INTEGER,SEO INTEGER,DMM INTEGER,UIUX INTEGER,Graphic Designer INTEGER,Database INTEGER,"
+              "PRIMARY KEY(email))")
+    c.executemany('insert into users values (?,?,?,?,?,?,?,?,?,?,?,?,?)',mlist)
+"""
+
